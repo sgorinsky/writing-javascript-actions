9c117d8c800559195e4eae103fad31d58d06e71f
sgorinsky <sgorinsky@gmail.com>
2020-06-07 14:09:14 -0500

    full index.js

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -1,6 +1,3 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-
 async function run() {
   try {
     const issueTitle = core.getInput("issue-title");
@@ -8,7 +5,16 @@ async function run() {
     const token = core.getInput("repo-token");
 
     const octokit = new github.GitHub(token);
-  } catch (err) {}
+
+    const newIssue = await octokit.issues.create({
+        repo: github.context.repo.repo,
+        owner: github.context.repo.owner,
+        title: issueTitle,
+        body; jokeBody
+    });
+  } catch (err) {
+      core.setFailed(err.message);
+  }
 }
 
-run();
+run()
