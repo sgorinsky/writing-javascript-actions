0c6862cdf5ada118c27fe679b8b517d68be88b54
sgorinsky <sgorinsky@gmail.com>
2020-06-07 14:24:56 -0500

    try to install @actions/github again

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -2,18 +2,18 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  const issueTitle = core.getInput("issue-title");
-  const catFact = core.getInput("joke");
-  const token = core.getInput("repo-token");
-
   try {
+    const issueTitle = core.getInput("issue-title");
+    const joke = core.getInput("joke");
+    const token = core.getInput("repo-token");
+    
     const octokit = new github.GitHub(token);
 
-    const newIssue = await octokit.issues.create({
+    await octokit.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
-      body: catFact
+      body: joke
     });
   } catch (error) {
     core.setFailed(error.message);
