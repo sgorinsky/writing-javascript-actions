aaf795bc54124625b56d08df5e0c1d5885a0dad3
sgorinsky <sgorinsky@gmail.com>
2020-06-07 14:20:05 -0500

    copy pasting code from other lab to see if that works

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -2,24 +2,24 @@ const core = require("@actions/core");
 const github = require("@actions/github");
 
 async function run() {
-  try {
-    const issueTitle = core.getInput("issue-title");
-    const jokeBody = core.getInput("joke");
-    const token = core.getInput("repo-token");
+  const issueTitle = core.getInput("issue-title");
+  const catFact = core.getInput("joke");
+  const token = core.getInput("repo-token");
 
-    console.log(github);
+  try {
     const octokit = new github.GitHub(token);
 
-    await octokit.issues.create({
+    const newIssue = await octokit.issues.create({
       repo: github.context.repo.repo,
       owner: github.context.repo.owner,
       title: issueTitle,
-      body: jokeBody
+      body: catFact
     });
-
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 
 run();
+
+
